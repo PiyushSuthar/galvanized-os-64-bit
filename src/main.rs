aa0254e64d16a57ad32afcddadbaf66aab084998
janaSunrise <warriordefenderz@gmail.com>
2021-07-02 16:59:55 +0530

    Worked on buffer.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,16 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+// Integrate modules
+mod vga_buffer;
+
 // Imports
 use core::panic::PanicInfo;
-
-// Static variables
-static HELLO: &[u8] = b"Hello World!";
 
 // Start function
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8; // Get the VGA Buffer
-
-    // Print the string.
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World{}", "!"); // Print using VGA Buffer
 
     // OS Loop
     loop {}
